92387e595c5a8d4fe0eaab130a81143aece814be
Raphael Majeed <Majeed@V4EMS-1140793.w2kroot.uni-oldenburg.de>
2018-07-12 21:30:25 +0200

    domain class added

--- lelantus-server/src/main/java/de/dzl/lelantus/server/Domain.java
+++ lelantus-server/src/main/java/de/dzl/lelantus/server/Domain.java
@@ -1,5 +1,13 @@
 package de.dzl.lelantus.server;
 
+/**
+ * Domains do not intersect with eachother. A domain can
+ * contain multiple sites. Within a single domain record linkage is 
+ * possible between the sites.
+ *
+ * @author R.W.Majeed
+ *
+ */
 public class Domain {
 
 }
